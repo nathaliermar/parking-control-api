7871d814a892500a5828f18dc1dd94668929f43a
nathaliermar <nathalie.rmartines@gmail.com>
2024-02-01 04:17:48 -0300

    Initial commit

---
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -0,0 +1,22 @@
+package com.api.parkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @SpringBootApplication -> Class that we are going to initiate the application
+@SpringBootApplication
+@RestController
+public class ParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlApplication.class, args);
+	}
+
+	@GetMapping("/") // http method
+	public String index(){
+
+		return "Hello World!!";
+	}
+}
